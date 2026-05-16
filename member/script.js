d08de222fa131ea67a18900bbdf2174e18340499
Rupsha jonokollan foundation <271165228+RJfoundation@users.noreply.github.com>
2026-05-16 18:57:23 +0600

    Update script.js

--- member/script.js
+++ member/script.js
@@ -33,7 +33,7 @@ const foundationMembers = [
         status: "active",
         image: "omor.webp",
         desc: "ফাউন্ডেশনের সকল প্রকার অনুদান এবং ব্যয়ের স্বচ্ছ হিসাব রক্ষণাবেক্ষণ করেন।",
-        memberid:"member id:RJF-2026-1390",
+        id: "member id:RJF-2026-1390",
         facebook: "https://www.facebook.com/fa.ruqe.75",
         whatsapp: "https://wa.me/"
     },
