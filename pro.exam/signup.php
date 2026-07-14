96446e275f11c892d61422e4c4544c6be3c535c3
IMRAN AHMED <imran.info.me@gmail.com>
2026-07-14 17:28:50 +0600

    Delete pro.exam directory

--- pro.exam/signup.php
+++
@@ -1,8 +0,0 @@
-<form action="signup_process.php" method="POST" enctype="multipart/form-data">
-    <input type="text" name="username" placeholder="আপনার নাম" required>
-    
-    <label>প্রোফাইল ফটো সিলেক্ট করুন:</label>
-    <input type="file" name="profile_image" accept="image/*" required>
-    
-    <button type="submit" name="signup">সাইন আপ</button>
-</form>
